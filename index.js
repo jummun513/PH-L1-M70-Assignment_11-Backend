c9e7caa745f3b28196239d55404c1a4e3399857e
jummun513 <jummunislam513@gmail.com>
2023-08-16 21:57:19 +0600

    test commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get('/', (req, res) => {
+    res.send("Server starts to running.");
+});
+
+app.get('/carData', (req, res) => {
+    res.send("");
+});
+
+app.listen(port, () => {
+    console.log('Listening to port', port);
+});
